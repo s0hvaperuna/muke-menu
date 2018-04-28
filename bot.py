cfbf2aced58b7e0c92e898f13ae7947ddcf29602
s0hvaperuna <tariqharb54@gmail.com>
2018-04-28 22:48:10 +0300

    Also sends a message to a channel

--- bot.py
+++ bot.py
@@ -7,8 +7,13 @@ from discord import File
 class GonaBot(Bot):
     async def on_ready(self):
         user = await self.get_user_info(176411695533522944)
+        p = os.path.join(os.getcwd(), 'ruokalista.png')
+        file = File(p, 'ruokalista.png')
         if user is not None:
-            p = os.path.join(os.getcwd(), 'ruokalista.png')
-            await user.send(file=File(p, 'ruokalista.png'))
-
+            await user.send(file=file)
+            
+        channel = self.get_channel(425730888538783745)
+        if channel is not None:
+            await channel.send(file=file)
+            
         await self.logout()
